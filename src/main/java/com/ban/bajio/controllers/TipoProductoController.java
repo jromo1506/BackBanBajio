cdf52f32156e0a82ae78dafe60e517f58cec268d
Jorge <jromo1506mx@gmail.com>
2026-01-11 00:56:29 -0600

    Exportar

--- src/main/java/com/ban/bajio/controllers/TipoProductoController.java
+++ src/main/java/com/ban/bajio/controllers/TipoProductoController.java
@@ -2,7 +2,12 @@ package com.ban.bajio.controllers;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +26,24 @@ public class TipoProductoController {
     }
 
 
+ 
+    // Listar todos los tipos de producto
     @GetMapping
-    public List<TipoProducto> listarTipos() {
-        return tipoProductoService.listarTodos();
+    public ResponseEntity<List<TipoProducto>> listarTipos() {
+        return ResponseEntity.ok(tipoProductoService.listarTipos());
+    }
+
+    // Crear tipo de producto
+    @PostMapping
+    public ResponseEntity<Void> crearTipo(@RequestBody TipoProducto tipoProducto) {
+        tipoProductoService.crearTipoProducto(tipoProducto.getNombre(), tipoProducto.getDescripcion());
+        return ResponseEntity.ok().build();
+    }
+
+    // Dar de baja tipo de producto
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> bajaTipo(@PathVariable Long id) {
+        tipoProductoService.bajaTipoProducto(id);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/ban/bajio/repository/TipoProductoRepository.java
+++ src/main/java/com/ban/bajio/repository/TipoProductoRepository.java
@@ -1,5 +1,7 @@
 package com.ban.bajio.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
